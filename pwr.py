778ddb4ac7712f7f82386a52ba152275c81c1690
drug5 <0x2102@gmail.com>
2022-10-13 14:48:52 +0200

    splitting date and time

--- pwr.py
+++ pwr.py
@@ -5,12 +5,14 @@ import json
 
 today = date.today()
 tomorrow = today + timedelta(days = 1)
-url = "https://api.energidataservice.dk/dataset/Elspotprices?start="+str(today)+"&end="+str(tomorrow)+"&filter={\"PriceArea\":\"DK1\"}"
+url = "https://api.energidataservice.dk/dataset/Elspotprices?start="+str(today)+"&end="+str(tomorrow)+"&filter={\"PriceArea\":\"DK1\"}&sort=HourDK%20asc"
 
+print(url)
 response = urlopen(url)
 data_json = json.loads(response.read())
 
 for d in data_json['records']:
   UnitPrice = d['SpotPriceDKK']/1000
   RoundUP = str(round(UnitPrice, 2))
-  print(d['HourDK'], RoundUP)
+  split = d['HourDK'].split("T")
+  print(split[0], split[1], RoundUP)
