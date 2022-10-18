852844c1dacd605430ab682fb470848a253517da
drug5 <0x2102@gmail.com>
2022-10-18 07:27:11 +0200

    Adding tax calculation

--- pwr.py
+++ pwr.py
@@ -4,7 +4,7 @@ from datetime import timedelta
 import json
 
 today = date.today()
-tomorrow = today + timedelta(days = 1)
+tomorrow = today + timedelta(days = 2)
 url = "https://api.energidataservice.dk/dataset/Elspotprices?start="+str(today)+"&end="+str(tomorrow)+"&filter={\"PriceArea\":\"DK1\"}&sort=HourDK%20asc"
 
 print(url)
@@ -13,6 +13,8 @@ data_json = json.loads(response.read())
 
 for d in data_json['records']:
   UnitPrice = d['SpotPriceDKK']/1000
-  RoundUP = str(round(UnitPrice, 2))
+  Tax = UnitPrice * 0.25
+  Total = UnitPrice + Tax
+  RoundUP = str(round(Total, 2))
   split = d['HourDK'].split("T")
   print(split[0], split[1], RoundUP)
