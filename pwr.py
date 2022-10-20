7674e1be70b66d62988cd141650512762c3ae7e0
drug5 <0x2102@gmail.com>
2022-10-20 10:09:29 +0200

    Adding tax calculation now taking times in to consideration.

--- pwr.py
+++ pwr.py
@@ -1,7 +1,13 @@
 from urllib.request import urlopen
 from datetime import date
 from datetime import timedelta
+import re
 import json
+
+#In Denmark transport fees are more expensive in some hours.
+transportCheap = 0.39
+transportExp = 1.03
+highFeeHours = ["17","18","19"]
 
 today = date.today()
 tomorrow = today + timedelta(days = 2)
@@ -12,9 +18,21 @@ response = urlopen(url)
 data_json = json.loads(response.read())
 
 for d in data_json['records']:
-  UnitPrice = d['SpotPriceDKK']/1000
-  Tax = UnitPrice * 0.25
-  Total = UnitPrice + Tax
-  RoundUP = str(round(Total, 2))
+  unitPrice = d['SpotPriceDKK']/1000
+  tax = unitPrice * 0.25
+  total = unitPrice + tax
+  roundUP = str(round(total,2))
+  time = re.sub(r'^.*?T','',d['HourDK'])
+  specific = re.sub(':.*','',time)
   split = d['HourDK'].split("T")
-  print(split[0], split[1], RoundUP)
+  if specific not in highFeeHours:
+    price = total + transportCheap
+    unitTax = price * 0.25
+    unitTotal = price + unitTax
+    print(split[0], split[1]+' Low fee! Price is '+str(unitTotal))
+  if specific in highFeeHours:
+    price = total + transportExp
+    unitTax = price * 0.25
+    unitTotal = price + unitTax
+    print(split[0], split[1]+' HIGH FEE! Price is '+str(unitTotal))
+  #print(split[0], split[1], roundUP)
